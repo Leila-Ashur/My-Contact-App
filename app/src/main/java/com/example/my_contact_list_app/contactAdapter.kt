ee2167b18260328555545baa4af0daffeb13bdac
leilaashur <leilahashur@gmail.com>
2023-06-15 00:17:17 +0300

    add commit

--- app/src/main/java/com/example/my_contact_list_app/contactAdapter.kt
+++ app/src/main/java/com/example/my_contact_list_app/contactAdapter.kt
@@ -3,9 +3,9 @@ package com.example.my_contact_list_app
 import android.media.Image
 
 data class ContactAdapter(
+    var image: String,
     var name: String,
-    var phoneNumber: String,
     var emailAddress:String,
-    var image: String
+    var phoneNumber: String
 
 )
